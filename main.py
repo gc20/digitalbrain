36e198cfb3a22946e1d9f29d9ed219a608380f6b
Govind Chandrasekhar <govind201@gmail.com>
2021-12-23 21:02:13 +0800

    Added process job + optimizations

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 ## Think
-# - Quality challenges at scale (markdown + extraction)
+# - Check quality at scale (crawl + process)
 # - Recos (tf-idf, better keyword n-grams, which keywords to tag?)
+# - Browsing history (threading across time, domain filtering, parsing view history)
+# - Chrome extension?
 # - Other (URL metadata? images?)
 
 ## Sample commands
 # python main.py --mode 'adhoc_crawl' --directory "/Users/Govind/Desktop/DB/" --adhoc_url "https://edition.cnn.com/2021/12/19/politics/joe-manchin-build-back-better/index.html"
 # python main.py --mode 'adhoc_process' --directory "/Users/Govind/Desktop/DB/" --adhoc_url "https://edition.cnn.com/2021/12/19/politics/joe-manchin-build-back-better/index.html"
-
+# python main.py --mode 'crawl_job' --directory "/Users/Govind/Desktop/DB/"
+# python main.py --mode 'process_job' --directory "/Users/Govind/Desktop/DB/"
+# 
 import argparse
 import requests
 import hashlib
 import os
 import validators
-import leveldb
+import leveldb # consider plyvel
 import time
 import json
 import newspaper # https://github.com/codelucas/newspaper
@@ -23,6 +27,7 @@ import bookmarks_parser
 import tqdm
 import random
 import tldextract
+import datetime
 # nltk.download('punkt')
 
 # Arguments
@@ -48,10 +53,10 @@ def crawl_store_url(url, html_path, crawl_log, crawl_override):
 
     # Override check
     if crawl_override is False and os.path.exists(url_path):
-        return 0, "URL html already exists for " + url_hash
+        return 0, "HTML pre-exists"
 
     # Crawl
-    response = requests.get(url)
+    response = requests.get(url, timeout=10)
     with open(url_path, "wb") as f:
         f.write(response.content)
 
@@ -59,16 +64,21 @@ def crawl_store_url(url, html_path, crawl_log, crawl_override):
     crawl_log.Put(url_hash.encode(encoding='UTF-8'), json.dumps({"s" : response.status_code, "u" : url, "t" : int(time.time())}).encode(encoding='UTF-8'))
     # crawl_log.Get(url_hash.encode(encoding='UTF-8')).decode()
 
-    return 1, "Written to " + url_hash
+    # Check status
+    if response.status_code != 200:
+        return 0, "HTTP status {}".format(response.status_code)
+
+    return 1, "Crawled!"
 
 # Parse to markdown from HTML
-def process_url(url, html_path, md_path, md_trail, process_log, url_metadata):
+def process_url(candidate, html_path, md_path, process_log):
 
     # Get HTML
+    url = candidate['url']
     url_hash = hashlib.md5(url.encode('utf-8')).hexdigest()
     url_path = os.path.join(html_path, url_hash + ".html")
-    if not os.path.exists(url_path):
-        return 0, "HTML doesn't exist"
+    if not os.path.exists(url_path): # swap with DB check?
+        0, "HTML doesn't exist"
     html = None
     with open(url_path, "rb") as f:
         html = f.read()
@@ -97,14 +107,14 @@ def process_url(url, html_path, md_path, md_trail, process_log, url_metadata):
     md_metadata = "- URL: " + url + "\n"
     if title:
         md_metadata += "- Article Title: " + title + "\n"
-    if url_metadata.get('title'):
-        md_metadata += "- Bookmark Title: " + url_metadata.get('title') + "\n"
+    if candidate.get('title'):
+        md_metadata += "- Bookmark Title: " + candidate.get('title') + "\n"
     if article.authors and len(article.authors):
         md_metadata += "- Authors: " + ", ".join(article.authors) + "\n"
     if article.publish_date:
         md_metadata += "- Publish Date: " + article.publish_date.strftime("%m/%d/%Y") + "\n"
-    if url_metadata.get('bdate'):
-        md_metadata += "- Bookmarked Date: " + datetime.datetime.fromtimestamp(int(url_metadata.get('bdate'))).strftime("%m/%d/%Y")
+    if candidate.get('bdate'):
+        md_metadata += "- Bookmarked Date: " + datetime.datetime.fromtimestamp(int(candidate.get('bdate'))).strftime("%m/%d/%Y")
     if article.keywords:
         md_metadata += "- Keywords: " + ", ".join(["[[" + k + "]]" for k in article.keywords]) + "\n"
     
@@ -120,11 +130,11 @@ def process_url(url, html_path, md_path, md_trail, process_log, url_metadata):
 
     # Chosen file
     md_filepath = md_path
-    if len(md_trail):
-        md_filepath = os.path.join(md_path, *md_trail)
+    if candidate.get('path', ""):
+        md_filepath = os.path.join(md_path, candidate.get('path', ""))
     if not os.path.exists(md_filepath):
         pathlib.Path(md_filepath).mkdir(parents=True, exist_ok=True) 
-    md_filename = os.path.join(md_filepath, url_metadata.get('path', ""), title + ".md")
+    md_filename = os.path.join(md_filepath, title + ".md")
 
     # Remove old file, write new file and store in process log
     url_hash_encoded = url_hash.encode(encoding='UTF-8')
@@ -143,13 +153,13 @@ def process_url(url, html_path, md_path, md_trail, process_log, url_metadata):
         print(md, file=f)
     process_log.Put(url_hash_encoded, json.dumps({"f" : md_filename, "u" : url, "t" : int(time.time())}).encode(encoding='UTF-8'))
 
-    return 1, title + ".md was created"
+    return 1, ".md was created"
 
 # [Internal] Parse chrome bookmarks
 def __parse_chrome_bookmarks(candidates, bookmarks, path):
     if bookmarks['type'] == 'folder':
         if bookmarks['title'] != 'Bookmarks Bar':
-            path += '/{}'.format(bookmarks['title'])
+            path += '{}/'.format(bookmarks['title'])
         for child in bookmarks.get('children', []):
             __parse_chrome_bookmarks(candidates, child, path)
     if bookmarks['type'] == 'bookmark':
@@ -170,35 +180,57 @@ def get_seed_candidates(input_path):
 
 # Run crawl job
 def run_crawl_job(candidates, html_path, crawl_log, crawl_override):
-    crawl_stats = {"total" : 0, "status" : {1: 0, 0: 0}}
+    crawl_stats = {"total" : 0, "status" : {1: 0, 0: 0}, "response" : {}}
     domain_stats = {}
     for candidate in tqdm.tqdm(candidates):
 
-        # Stats/logs
-        tldextract = tldextract.extract(candidate['url'])
-        domain = tldextract.domain + "." + tldextract.suffix
+        # Stats 1
+        tld_extract = tldextract.extract(candidate['url'])
+        domain = tld_extract.domain + "." + tld_extract.suffix
         if domain not in domain_stats:
-            domain_stats[domain] = {"total" : 0, "status" : {1: 0, 0: 0}}
+            domain_stats[domain] = {"total" : 0, "status" : {1: 0, 0: 0}, "response" : {}}
         crawl_stats['total'] += 1
         domain_stats[domain]['total'] += 1
         print(candidate)
         if crawl_stats['total'] % 10 == 0:
             print(crawl_stats)
-        status = 0
+        if crawl_stats['total'] % 100 == 0:
+            print(domain_stats, flush=True)
+        status, response = 0, ""
 
         # Crawl
         try:
-            status, _ = crawl_store_url(candidate['url'], os.path.join(args.directory, "prod", "html"), crawl_log, crawl_override)
+            status, response = crawl_store_url(candidate['url'], os.path.join(args.directory, "prod", "html"), crawl_log, crawl_override)
         except Exception as e:
-            pass
+            response = str(e)[0:50]
+
+        # Stats 2
         crawl_stats['status'][status] += 1
         domain_stats[domain]['status'][status] += 1
+        crawl_stats['response'][response] = 1 if response not in crawl_stats['response'] else crawl_stats['response'][response]+1
+        domain_stats[domain]['response'][response] = 1 if response not in domain_stats[domain]['response'] else domain_stats[domain]['response'][response]+1
+    
     crawl_stats['success_rate'] = "0%" if crawl_stats['total'] == 0 else "%.2f%" % (crawl_stats['stats'][1] / crawl_stats['total'])
     print(domain_stats)
     return 1, json.dumps(crawl_stats)
 
 
 # Run process job
+def run_process_job(candidates, html_path, md_path, process_log):
+    process_stats = {"total" : 0, "status" : {1: 0, 0: 0}, "response" : {}}
+    for candidate in tqdm.tqdm(candidates):
+        status, response = 0, ""
+        try:
+            process_stats['total'] += 1
+            status, response = process_url(candidate, html_path, md_path, process_log)
+        except Exception as e:
+            response = str(e)[0:50]
+        process_stats['status'][status] += 1
+        process_stats['response'][response] = 1 if response not in process_stats['response'] else process_stats['response'][response]+1
+        if process_stats['total'] % 100 == 0:
+            print(process_stats)
+    return 1, json.dumps(process_stats)
+
 
 # Main file
 if __name__ == "__main__":
@@ -216,14 +248,14 @@ if __name__ == "__main__":
             status, response = crawl_store_url(args.adhoc_url, os.path.join(args.directory, "prod", "html"), crawl_log, args.crawl_override)
 
         elif args.mode == 'adhoc_process':
-            status, response = process_url(args.adhoc_url, os.path.join(args.directory, "prod", "html"), os.path.join(args.directory, "prod", "md"), [], process_log, {})
+            status, response = process_url({"url" : args.adhoc_url}, os.path.join(args.directory, "prod", "html"), os.path.join(args.directory, "prod", "md"), process_log)
 
         elif args.mode == 'crawl_job':
             candidates = get_seed_candidates(os.path.join(args.directory, "input"))
             status, response = run_crawl_job(candidates, os.path.join(args.directory, "prod", "html"), crawl_log, args.crawl_override)
         elif args.mode == 'process_job':
             candidates = get_seed_candidates(os.path.join(args.directory, "input"))
-            # status, response = process_url(args.url, os.path.join(args.directory, "prod", "html"), os.path.join(args.directory, "prod", "md"), [], process_log)
+            status, response = run_process_job(candidates, os.path.join(args.directory, "prod", "html"), os.path.join(args.directory, "prod", "md"), process_log)
 
     except Exception as e:
         status, response = 0, str(e)
